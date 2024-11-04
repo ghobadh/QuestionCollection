ea4c1bca43fc68dbba7641d06906688c96877f47
ghobadh <ghobadh@gmail.com>
2024-11-04 12:43:55 -0500

    Initial Commit

---
+++ src/main/java/ca/gforcesoftware/questioncollection/QuestionCollectionApplication.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.questioncollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gforcesoftware/questioncollection/QuestionCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.questioncollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
